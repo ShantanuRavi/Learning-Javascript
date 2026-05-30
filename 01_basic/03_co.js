6e70ea6b8f696894cf63a2b29cfbfe5617ccdef0
ShantanuRavi <shaanravi133@gmail.com>
2026-05-30 12:42:37 +0000

    conversion

---
+++ 01_basic/03_co.js
@@ -0,0 +1,13 @@
+//  conversion operation
+
+let score = undefined
+
+let valueInNumber = Number(score)
+
+console.log(typeof(valueInNumber));
+
+console.log(valueInNumber);
+
+//"33" => 33
+//"33abs" => NaN
+// true/false => 1/0
