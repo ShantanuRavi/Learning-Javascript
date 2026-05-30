a500e297280a1177985e40b8af90df28a19edeea
ShantanuRavi <shaanravi133@gmail.com>
2026-05-30 12:27:26 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict" // treat all js code as newer version 
+
+
+let name = "Raj"
+let age = 23
+let isLoggedIn = true
+let state 
+
+
+//datatypes in javascript
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique 
+// object
+
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(isLoggedIn));
+console.log(typeof(state));
+console.log(typeof(null));
+console.log(typeof(undefined));
+
