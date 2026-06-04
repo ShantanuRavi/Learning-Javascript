29065d57195228ed46d608520bd9c006741b870a
ShantanuRavi <shaanravi133@gmail.com>
2026-06-04 19:37:13 +0000

    datatypes end here

---
+++ 01_basic/04_datatypes_summary.js
@@ -0,0 +1,37 @@
+// primitive datatypes
+
+//it  has 7 types 
+//->string
+//->number
+//->boolean
+//->null
+//->undefined
+//->symbol 
+//->bigInt
+
+
+// Non primitive (referance) datatype
+
+//->array
+//->object
+//->functions
+
+//array
+
+const alpha =["A","B","C"]
+
+//object
+
+let myObj = {
+    name:"Ravi",
+    age:"23"
+}
+
+//function
+
+let myFunction = function()
+{
+    console.log(alpha);
+    console.log(myObj);    
+}
+myFunction()
