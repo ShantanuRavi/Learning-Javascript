7d795e355359f1d711dea9fe5d61ee79bb065e9c
ShantanuRavi <shaanravi133@gmail.com>
2026-06-09 15:15:35 +0000

    string complete

---
+++ 01_basic/05_string.js
@@ -0,0 +1,46 @@
+// let text = "John Doe";
+// console.log(text);
+
+
+//----USING STRING WITH SINGLE OR DOUBLE QUOTES----
+
+// let carName1 = "Volvo XC60";  // Double quotes
+// let carName2 = 'Maruti';  // Single quotes
+// console.table([carName1,carName2])
+
+
+//----QUOTES INSIDE QUOTES----
+
+// let answer1 = "It's alright";
+// let answer2 = "He is called 'Johnny'";  single quotes inside double quotes
+// let answer3 = 'He is called "Johnny"';  double qoutes inside single quotes
+// console.table(answer1)
+// console.table(answer2)
+// console.table(answer3)
+
+
+//----TEMPLATE STRING----
+
+// let name=`Rahul`
+// const age=23
+// console.log(`My name is ${name} and i am ${age} years old.`)
+
+
+//----CHARACTER LIKE \ OR "" OR '' INSIDE STRING----
+
+//let text = "We are the so-called "Vikings" from the north.";    THIS WILL GIVE ERROR SO WE USE \
+// let text = "We are the so-called \"Vikings\" from the north.";
+// console.log(text)
+
+//----JAVASCRIPT STRINGS AS OBJECTS----
+
+// let x = "John";
+// let y = new String("John");
+// console.log(x)
+// console.log(y)
+
+
+//----STRING METHODS / FUNCTIONS----
+
+let str = new String("Ravi")
+console.log(str[0])
