0454b920f5871fcf086567bd8f242e125df9f3ee
Shantanu Ravi <shaanravi133@gmail.com>
2026-06-17 13:41:28 +0000

    for of loop

---
+++ 03_iterables/01_for_of_loop.js
@@ -0,0 +1,19 @@
+// const arr = [1,2,3,4,5]
+// for (const num of arr) {
+//   console.log(num)
+// }
+
+// for(const ch of "hello")
+// {
+//   console.log(ch)
+// }
+
+// const m = new Map()
+// {
+//    m.set("IN","India")
+//    m.set("USA","United States Of America")  
+//    m.set("Fr","France")
+// }
+// for(const [key,value] of m){
+//   console.log(key,value)
+// }
