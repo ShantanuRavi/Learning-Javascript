1e269e94b9e26808d148d7cea3c30f789099202a
Shantanu Ravi <shaanravi133@gmail.com>
2026-07-04 10:55:30 +0000

    events done

--- 06_events/01_events.js
+++ 06_events/01_events.js
@@ -1,2 +1,10 @@
-const im = document.getElementById("horse")
-console.log(im)
+//-------- PROJECT REMOVE A PICTURE BY CLICKING ON IT --------//
+
+document.querySelector("#images").addEventListener("click",function(e){
+  if(e.target.tagName === "IMG")
+  {
+    const temp = e.target.parentNode;
+    console.log(`${e.target.id} REMOVED`)
+    temp.remove();
+  }
+})
